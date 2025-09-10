50dfcb99be58a14d0d67df0d078a2ecfea1dcd3f
LFPerylo <lfpb@cesar.school>
2025-09-10 15:26:00 -0300

    deletando pastas

--- pipeline-distribuido/utils/config.py
+++
@@ -1,3 +0,0 @@
-HOST = '127.0.0.1'      # Endereço do servidor (localhost)
-PORT = 5555             # Porta de comunicação
-TAMANHO_FILA = 10       # Capacidade máxima das filas entre threads/processos
